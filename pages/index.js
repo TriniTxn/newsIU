e85f5e36ca4054fb2310bedccf086902c2e94809
Giovanni dos Santos Gomes <136286261+TriniTxn@users.noreply.github.com>
2026-03-30 04:04:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Spruce and Rosewood are better than Mahogany</h1>
+  return <h1>Spruce and Rosewood are better than Mahogany</h1>;
 }
 
-export default Home
+export default Home;
